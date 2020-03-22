dac47678a78400a46a9169b0cece1aae251dcbbe
Jan L <13069324+devjalo@users.noreply.github.com>
2020-03-22 03:04:53 +0100

    allow tls inbound traffic

--- terraform/security.tf
+++ terraform/security.tf
@@ -18,3 +18,28 @@ resource "aws_security_group" "ingress-all-test" {
       "0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.kiez-env.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = aws_vpc.kiez-env.cidr_block
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
