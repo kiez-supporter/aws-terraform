888c6a276a2f287c75e3f917c3678f04f155c565
Jan L <13069324+devjalo@users.noreply.github.com>
2020-03-22 03:12:05 +0100

    update cidr_blocks value to required list of string type

--- terraform/security.tf
+++ terraform/security.tf
@@ -29,7 +29,7 @@ resource "aws_security_group" "allow_tls" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = aws_vpc.kiez-env.cidr_block
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
   egress {
