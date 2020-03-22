f993ffde34fe9aa01c4fc85aafc602af4586a551
Jan L <13069324+devjalo@users.noreply.github.com>
2020-03-22 03:32:37 +0100

    add security-groups for tls, http. add to instance

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "ingress-all-test" {
+resource "aws_security_group" "allow-ssh" {
   name = "allow-all"
   vpc_id = aws_vpc.kiez-env.id
   ingress {
@@ -41,5 +41,26 @@ resource "aws_security_group" "allow_tls" {
 
   tags = {
     Name = "allow_tls"
+  }
+}
+
+resource "aws_security_group" "http" {
+  name = "http"
+  vpc_id = aws_vpc.kiez-env.id
+  ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+  }
+  // Terraform removes the default rule
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"]
   }
 }
